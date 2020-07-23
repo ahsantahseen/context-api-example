448080832206bdbb45b8207b497a97f4746acaef
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-23 17:47:13 +0500

    2nd Commit

--- src/Movie.js
+++ src/Movie.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React from "react";
+
+const Movie = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.price}</h3>
+    </div>
+  );
+};
+export default Movie;
